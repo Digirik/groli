0b03989ead2b9531e5351237108eadc194403e12
digirik <erikerbar@web.de>
2020-12-30 00:13:47 +0100

    Make project intellij ready

---
+++ groli-rest-app/src/main/java/de/digirik/groli/GroliApplication.java
@@ -0,0 +1,13 @@
+package de.digirik.groli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroliApplication.class, args);
+	}
+
+}
